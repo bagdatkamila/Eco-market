f973d56ad24040e540c198d83b141efa3684427a
Alishkoo <baisholanov.alibek@gmail.com>
2024-05-15 13:52:15 +0500

    fix bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript{
 
     }
 }
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
